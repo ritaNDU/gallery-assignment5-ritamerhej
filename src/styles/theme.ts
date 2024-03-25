9d73db438a5cabd124980554cda9ad4caef0a260
rita <rita.merhej@hotmail.com>
2024-03-25 13:10:39 +0200

    feat/ add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const NORMAL_FONT_SIZE = 20;
+
+const theme = {
+  colors: {
+    primary: '',
+    secondary: '',
+    backgroundColor: '#F0F3F4',
+    textColor: '#000',
+    favoritesColor: '',
+    favoritesTextColor: '',
+  },
+  fontSize: {
+    small: (NORMAL_FONT_SIZE * 75) / 100,
+    normal: NORMAL_FONT_SIZE,
+    large: NORMAL_FONT_SIZE * 1.5,
+  },
+};
+
+export default theme;
